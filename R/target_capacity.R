155c8b08f14e2fc3bbc867f0d4f4db889ce0ce34
PeterSNHS <peter.shakeshaft1@nhs.net>
2023-12-07 11:12:34 +0000

    Added examples and metadata to target_capacity and target_queue_size. Renamed misleading variable mean_wait to target_wait in target_capacity.

--- R/target_capacity.R
+++ R/target_capacity.R
@@ -1,18 +1,28 @@
-#' @title Title
+#' @title Target Capacity
 #'
 #' @description
-#' A short description...
+#' Calculates the target capacity to achieve a given target waiting time as a 
+#' function of observed demand, target waiting time and a variability of demand 
+#' parameter F.
+#' 
+#' F defaults to 1.
+#' 
+#' Target Capacity = Demand + 2 * ( 1 + 4 * F ) / Target Wait
 #'
 #' @param demand
-#' @param mean_wait
+#' @param target_wait
 #'
-#' @return
+#' @return target capacity to achieve a target waiting time
 #' @export
 #'
 #' @examples
+#' 
+#' If the target wait is 52 weeks, demand is 30 patients per week and F = 3 then
+#' Target capacity = 30 + 2*(1+4*3)/52 = 30.5 patients per week.
+#' 
+#' target_capacity(30,52,3)
 #'
-#'
-target_capacity <- function(demand, mean_wait, F = 1) {
-  target_cap <- demand +  2 * ( 1 + 4 * F ) / mean_wait
+target_capacity <- function(demand, target_wait, F = 1) {
+  target_cap <- demand +  2 * ( 1 + 4 * F ) / target_wait
   return(target_cap)
 }
--- R/target_queue_size.R
+++ R/target_queue_size.R
@@ -1,18 +1,31 @@
-#' @title Title
+#' @title Target Queue Size
 #'
 #' @description
-#' A short description...
+#' Uses Little's Law to calculate the target queue size to achieve a target mean 
+#' waiting time as a function of observed demand,  target wait and a factor used 
+#' in the target mean waiting time calculation.
+#' 
+#' Target Queue Size = Demand * Target Wait / 4.
+#'
+#' The average wait should sit somewhere between 
+#' target_wait/factor=6 < Average Waiting Time < target_wait/factor=4
+#' The factor defaults to 4.
+#' 
+#' Only applicable when Capacity > Demand.
 #'
 #' @param demand
 #' @param target_wait
 #' @param factor
 #'
-#' @return
+#' @return target queue length
 #' @export
 #'
 #' @examples
+#' If demand  is 30 patients per week and the target wait is 52 weeks, then the 
+#' Target queue size = 30 * 52/4 = 390 patients.
 #'
-#'
+#' target_queue_size(30,52,4)
+#' 
 target_queue_size <- function(demand, target_wait, factor = 4) {
   mean_wait <- average_wait(target_wait, factor)
   target_queue_length <- demand * mean_wait
