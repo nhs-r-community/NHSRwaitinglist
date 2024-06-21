42b1078efb7511dd6c1458e1d68b54c67c218a3b
neilwalton <53338269+neilwalton@users.noreply.github.com>
2024-06-21 12:14:06 +0100

    withdrawal of treatment added to wl_schedule

--- R/wl_schedule.R
+++ R/wl_schedule.R
@@ -24,26 +24,91 @@
 #' schedule <- c.Date("2024-01-03", "2024-01-05", "2024-01-18")
 #' updated_waiting_list <- wl_schedule(waiting_list, schedule)
 #'
+#' # Or if there are withdrawals from the list too:
+#' referrals <- c.Date("2024-01-01", "2024-01-04", "2024-01-10", "2024-01-16")
+#' removals <- c.Date("2024-01-08", NA, NA, NA)
+#' withdrawal <- c.Date(NA,NA,"2024-01-14",NA)
+#' waiting_list <- data.frame("referral" = referrals, "removal" = removals, "withdrawal" = withdrawal)
+#' schedule <- c.Date("2024-01-03", "2024-01-05", "2024-01-18")
+#' updated_waiting_list <- wl_schedule(waiting_list, schedule)
+#'
 #' # TODO ALLOW:
 #' # schedule to be a dataframe or vector
 wl_schedule <- function(
     waiting_list,
     schedule,
-    referral_index = 1,
-    removal_index = 2) {
+    referral_index = NA,
+    removal_index = NA,
+    withdrawal_index = NA) {
+
+  if( !is.na(withdrawal_index) ){
+    withdrawals = TRUE
+  }
+
+  # guess withdraw, referral and removal index if not given
+  if( is.na(withdrawal_index)
+     && "withdrawal" %in% colnames(waiting_list) ){
+    withdrawals = TRUE
+    withdrawal_index <- which(colnames(waiting_list) == "withdrawal")
+  }
+
+  if( is.na(referral_index)
+      && "referral" %in% colnames(waiting_list) ){
+    referral_index <- which(colnames(waiting_list) == "referral")
+  }
+
+  if( is.na(removal_index)
+      && "removal" %in% colnames(waiting_list) ){
+    removal_index <- which(colnames(waiting_list) == "removal")
+  }
+
   # split waiters and removed
   wl <- waiting_list[is.na(waiting_list[, removal_index]), ]
   wl_removed <- waiting_list[!(is.na(waiting_list[, removal_index])), ]
   rownames(wl) <- NULL
 
   # schedule
-  i <- 1
-  for (op in as.list(schedule)) {
-    if (op > wl[i, referral_index] && i <= nrow(wl)) {
-      wl[i, removal_index] <- as.Date(op)
-      i <- i + 1
+  if (!withdrawals){
+    i <- 1
+    for (op in as.list(schedule)) {
+      if (op > wl[i, referral_index] && i <= nrow(wl)) {
+        wl[i, removal_index] <- as.Date(op)
+        i <- i + 1
+      }
     }
   }
+
+  # schedule checking for withdrawals
+  if(withdrawals){
+
+    for ( i in 1:length(schedule) ){
+      for ( j in 1:nrow(wl) ){
+        cat(i,j,"\n")
+        if ( schedule[i] > wl[j, referral_index]
+             && is.na(wl[j,removal_index]) ){
+          if ( !is.na(wl[j,withdrawal_index])){
+            if ( schedule[i] < wl[j, withdrawal_index]) {
+                wl[j,removal_index] <- schedule[i]
+              break
+            }
+          } else {
+            wl[j,removal_index] <- schedule[i]
+            break
+          }
+        }
+      }
+    }
+
+    # update removals that were because withdrawn
+    for ( j in 1:nrow(wl) ){
+      if(is.na(wl[j,removal_index])
+         && !is.na(wl[j,withdrawal_index]) ) {
+        wl[j,removal_index] <- wl[j,withdrawal_index]
+      }
+    }
+
+  }
+
 
   # recombine to update list
   updated_list <- rbind(wl_removed, wl)
