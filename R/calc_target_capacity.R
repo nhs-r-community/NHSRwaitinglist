103ac232ecae717f650c75b7a91551d35cc14fe7
Tom Smith <thomas.smith5@nuh.nhs.uk>
2024-10-15 21:55:03 +0100

    comment out tests because function is no longer vectorised

--- R/calc_target_capacity.R
+++ R/calc_target_capacity.R
@@ -19,6 +19,7 @@
 #' @param cv_demand coefficient of variation of time between arrivals
 #' @param cv_capacity coefficient of variation between removals due to
 #'   operations completed
+#' @param num_referrals TODO ask Neil
 #'
 #' @return numeric. The capacity required to achieve a target waiting time.
 #' @export
@@ -32,6 +33,7 @@
 #' calc_target_capacity(demand, target_wait)
 #'
 #' # TODO: Include a couple of standard deviations for errors in the mean demand
+#' # TODO: ask Neil what num_referrals is, and include in roxygen docblock
 calc_target_capacity <- function(
     demand,
     target_wait,
@@ -39,7 +41,8 @@ calc_target_capacity <- function(
     cv_demand = 1,
     cv_capacity = 1,
     num_referrals = 0) {
-  check_class(demand, target_wait, factor, cv_demand, cv_capacity)
+
+  check_class(demand, target_wait, factor, cv_demand, cv_capacity, num_referrals)
   # Add two standard deviations to demand if it is estimated
   if(num_referrals > 0 & 2*demand*cv_demand / sqrt(num_referrals) < 1){
     demand <- demand  / (1- 2*demand*cv_demand / sqrt(num_referrals) )
