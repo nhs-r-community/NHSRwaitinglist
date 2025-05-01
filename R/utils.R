13eaa85f9234cb15b41fe8418c6ece939147875b
David Foord <140633967+davidfoord1@users.noreply.github.com>
2025-05-01 17:02:55 +0100

    Add further supported classes to check_class
    - Adds support for logical, data.frame, Date, and NULL.
    - Replaces the switch statement between is functions with `is()` method dispatch.
    - Still keeping a specified list in `supported_classes` to help catch typos etc.

--- R/utils.R
+++ R/utils.R
@@ -6,23 +6,20 @@
 #'     called.
 #' @noRd
 check_class <- function(
-  ...,
-  .expected_class = c("numeric", "character"),
-  .call = rlang::caller_env()
-) {
-  .expected_class <- match.arg(.expected_class)
+    ...,
+    .expected_class = "numeric",
+    .call = rlang::caller_env()) {
+
+  # expected types included NULL for argument defaults
+  supported_classes <- c("numeric", "character", "logical", "data.frame",
+                         "Date", "NULL")
+
+  .expected_class <- match.arg(.expected_class, supported_classes)
 
   args <- rlang::dots_list(..., .named = TRUE)
 
-  args_are_class <- lapply(
-    args,
-    function(arg) {
-      switch(.expected_class,
-        numeric   = is.numeric(arg),
-        character = is.character(arg),
-      )
-    }
-  )
+  args_are_class <- lapply(args,
+                           \(arg) methods::is(arg, .expected_class))
 
   fails_names <- names(Filter(isFALSE, args_are_class))
 
