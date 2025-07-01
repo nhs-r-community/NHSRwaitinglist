13d8df7b650a4447a8fc3cac2a52255e2a466af1
Chris Mainey <c.mainey1@nhs.net>
2025-07-01 10:34:44 +0100

    Corrected error introduced by linebreak.

--- tests/testthat/test-sim_patients.R
+++ tests/testthat/test-sim_patients.R
@@ -59,7 +59,7 @@ test_that("sim_patients generates random names for patients", {
 test_that("sim_patients fails gracefully wiwhen given zero rows", {
   #result <- sim_patients(n_rows = 0)
   #expect_equal(nrow(result), 0)
-  expect_error(sim_patients(n_rows = 0)
-               , "NOTE: Please supply a positive integer for the argument
-               n_rows.")
+  expect_error(
+    sim_patients(n_rows = 0)
+    , "NOTE: Please supply a positive integer for the argument n_rows.")
 })
