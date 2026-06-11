671f832ffaa7cb38fa691c4b4e66c12b7f029dc3
Tom Smith <thomas.smith103@nhs.net>
2026-06-11 15:57:07 +0100

    styling changes, and replacing magrittr pipe

--- R/rtt_improvement.R
+++ R/rtt_improvement.R
@@ -1,15 +1,18 @@
 #' Create a ggplot of an RTT improvement journey
 #'
 #' @param referral_rate numeric. A weekly referral rate to plot
-#' @param rtt_performance numeric vector. A vector of performance thresholds, as proportion e.g. 0.65 = 65%.
-# '  Default values are: c(0.5, 0.60, 0.70, 0.80, 0.92)
-#' @param n_bins numeric.  Number of bins for histogram. Default is 1 - 64 weeks.
+#' @param rtt_performance numeric vector. A vector of performance thresholds,
+#' as proportion e.g. 0.65 = 65%.
+#' Default values are: c(0.5, 0.60, 0.70, 0.80, 0.92)
+#' @param n_bins numeric.  Number of bins for histogram.
+#' Default is 1 - 64 weeks.
 #' @returns ggplot object
 #'
 #' @importFrom stats pexp dexp
 #' @export
 #'
-plot_rtt_journey <- function(referral_rate, rtt_performance = c(0.5, 0.60, 0.70, 0.80, 0.92),
+plot_rtt_journey <- function(referral_rate,
+                             rtt_performance = c(0.5, 0.60, 0.70, 0.80, 0.92),
                              n_bins = 64) {
   # rtt performance levels to include in the plot
   # rtt_performance <- c(0.5, 0.60, 0.70, 0.80, 0.92)
@@ -22,7 +25,9 @@ plot_rtt_journey <- function(referral_rate, rtt_performance = c(0.5, 0.60, 0.70,
 
   params <- data.frame(
     lambda = lambda_values,
-    total_population = referral_rate / lambda_values, # ensures lambda * population = referral_rate
+
+    # ensures lambda * population = referral_rate
+    total_population = referral_rate / lambda_values,
     percent_within_target = 100 * pexp(target_weeks, rate = lambda_values)
   )
 
@@ -33,42 +38,61 @@ plot_rtt_journey <- function(referral_rate, rtt_performance = c(0.5, 0.60, 0.70,
   df <- expand.grid(
     x = seq(0, n_bins, length.out = n_bins * 100),
     lambda = lambda_values
-  ) %>%
-    dplyr::left_join(params, by = "lambda") %>%
+  ) |>
+    dplyr::left_join(params, by = "lambda") |>
     dplyr::mutate(
       density = dexp(.data$x, rate = .data$lambda),
       population_y = .data$density * .data$total_population
     )
 
   # create a binned data summary ready for histogram plotting
-  df_binned <- df %>%
-    dplyr::mutate(bin = ggplot2::cut_number(.data$x, n_bins)) %>%
-    dplyr::group_by(.data$lambda, .data$total_population, .data$percent_within_target, .data$bin) %>%
+  df_binned <- df |>
+    dplyr::mutate(bin = ggplot2::cut_number(.data$x, n_bins)) |>
+    dplyr::group_by(
+      .data$lambda,
+      .data$total_population,
+      .data$percent_within_target,
+      .data$bin
+    ) |>
     dplyr::summarise(
       height = mean(.data$population_y),
       .groups = "drop"
-    ) %>%
+    ) |>
     dplyr::mutate(
-      x_min = as.numeric(stringr::str_extract(.data$bin, "\\d+(?=,)")), # get the first digit from the bin factor
+      # get the first digit from the bin factor
+      x_min = as.numeric(stringr::str_extract(.data$bin, "\\d+(?=,)")),
       label = forcats::fct_reorder(
-        paste0(format(round(.data$total_population, 0), big.mark = ","), " patients = ", .data$percent_within_target, "% RTT"),
+        paste0(
+          format(round(.data$total_population, 0), big.mark = ","),
+          " patients = ",
+          .data$percent_within_target,
+          "% RTT"
+        ),
         dplyr::desc(.data$total_population)
       )
     )
 
   # build a dataframe to drive a plot annotation
-  annotation_df <- df_binned %>%
-    dplyr::select(.data$label) %>%
+  annotation_df <- df_binned |>
+    dplyr::select(.data$label) |>
     dplyr::distinct()
 
   # define the colours needed
   colours <- c("#DEEBF7", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5")
 
   # build the plot
-  p <- ggplot2::ggplot(df_binned, ggplot2::aes(x = .data$x_min, y = .data$height, fill = .data$label)) +
+  p <- ggplot2::ggplot(
+    df_binned,
+    ggplot2::aes(x = .data$x_min, y = .data$height, fill = .data$label)
+  ) +
     ggplot2::scale_fill_manual(values = colours) +
-    ggplot2::geom_col(position = "identity", just = 0) + # overlaid semi-transparent bars
-    ggplot2::geom_vline(xintercept = target_weeks, colour = "red", linetype = "dashed") +
+    # overlaid semi-transparent bars
+    ggplot2::geom_col(position = "identity", just = 0) +
+    ggplot2::geom_vline(
+      xintercept = target_weeks,
+      colour = "red",
+      linetype = "dashed"
+    ) +
 
     # red target box
     ggplot2::annotate("rect",
@@ -88,7 +112,13 @@ plot_rtt_journey <- function(referral_rate, rtt_performance = c(0.5, 0.60, 0.70,
     ) +
     ggplot2::labs(
       title = "Waiting List Size and Corresponding RTT Performance Levels",
-      subtitle = paste0("For referral rate of ", referral_rate, " patients/wk \nAssuming a typical exponential waiting time distribution"),
+      subtitle = paste0(
+        "For referral rate of ",
+        referral_rate,
+        " patients/wk \n",
+        "Assuming a typical exponential",
+        " waiting time distribution"
+      ),
       caption = "Source: {NHSRwaitinglist} R package",
       x = "Weeks waiting",
       y = "Number of patients (or pathways)",
@@ -113,7 +143,11 @@ rep_rtt_improvement <- function(referral_rates) {
   dir.create(stage_dir, recursive = TRUE)
 
   # copy template into staging (helps with relative paths, resources, etc.)
-  template_qmd <- system.file("quarto", "rtt_improvement_report.qmd", package = "NHSRwaitinglist")
+  template_qmd <- system.file(
+    "quarto",
+    "rtt_improvement_report.qmd",
+    package = "NHSRwaitinglist"
+  )
 
   stage_qmd <- file.path(stage_dir, basename(template_qmd))
   file.copy(template_qmd, stage_qmd, overwrite = TRUE)
@@ -135,7 +169,12 @@ rep_rtt_improvement <- function(referral_rates) {
 
   # copy to user destination
   ok <- file.copy(rendered_file, here::here(), overwrite = TRUE)
-  if (!ok) stop("Failed to copy rendered report to user_filepath.", call. = FALSE)
+  if (!ok) {
+    stop(
+      "Failed to copy rendered report to user_filepath.",
+      call. = FALSE
+    )
+  }
 
   return(
     file.path(here::here(), basename(rendered_file))
